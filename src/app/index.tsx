0c5513f9e99657252ef3b80b93e2d6782e8292f0
Shawn Law <shawnlaw6669@gmail.com>
2025-05-28 11:49:24 +0800

    style the markers abit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
 import { dummy } from '@assets/data/dummy';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} >
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          longitude: 100.497027,
+          latitude: 13.759030,
+          latitudeDelta: 0.0922, 
+          longitudeDelta: 0.0421,
+        }}
+      >
         {dummy.map((item) => (
           <Marker
             key={item.id}
@@ -16,7 +25,11 @@ export default function App() {
             }}
             title={item.title}
             description={item.price.toString()}
-          />
+          >
+            <View style={styles.priceContainer}>
+              <Text style={styles.priceText}>RM {item.price}</Text>
+            </View>
+          </Marker>
         ))}
       </MapView>
     </View>
@@ -31,4 +44,26 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  priceContainer: {
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: 'grey'
+
+    // shadows
+    // shadowColor: "#000",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 2,
+    // },
+    // shadowOpacity: 0.25,
+    // shadowRadius: 3.84,
+    // elevation: 5,
+  },
+  priceText: {
+    fontWeight: 'bold',
+  }
+  
 });
