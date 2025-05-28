5e836c1e898ee5358dd75284c0e779f9a2ec6b0b
Shawn Law <shawnlaw6669@gmail.com>
2025-05-28 18:28:57 +0800

    yay! the React Native Bottom Sheet is working!

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { dummy } from '@assets/data/dummy';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import CustomMarker from '@/components/CustomMarker';
 import ApartmentListItem from '@/components/ApartmentListItem';
+import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 export default function MapsScreen() {
   const [selectedApartment, setSelectedApartment] = useState(null)
+  const snapPoints = useMemo(() => ['25%', '50%'], [])
 
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{title: 'Maps'}} />
       <MapView 
         style={styles.map} 
         provider={PROVIDER_GOOGLE}
@@ -28,6 +33,16 @@ export default function MapsScreen() {
       {selectedApartment && (
         <ApartmentListItem apartment={selectedApartment}/>
       )}
+
+      <BottomSheet
+        // ref={bottomSheetRef}
+        snapPoints={snapPoints}
+        // onChange={handleSheetChanges}
+      >
+        <BottomSheetView style={{}}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   );
 }
